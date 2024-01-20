67e65f4e70a23463b2c61a961810d1c19db33ebb
Aayush Pandey <aayushpandey1100@gmail.com>
2024-01-21 01:13:12 +0530

    order in descending

--- controllers/posts.js
+++ controllers/posts.js
@@ -29,7 +29,9 @@ export const createPost = async (req, res) => {
 /* READ */
 export const getFeedPosts = async (req, res) => {
   try {
-    const post = await Post.find();
+    const post = await Post.aggregate([
+      { $sort: { createdAt: -1 } } // Sort by createdAt in descending order
+    ]);
     res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
